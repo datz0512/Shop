77b9a2d975e4e4382b89601ead8f52890a14aa47
datz <datblu2003@gmail.com>
2023-11-02 16:53:55 +0700

    add nodemailer

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,38 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+const { google } = require("googleapis");
 
 const User = require("../models/user");
+
+const CLIENT_ID =
+    "860935289119-kao17rrd80pasag0bl14g6hgj8e6appv.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+    "1//04k5yaws6K5utCgYIARAAGAQSNwF-L9Iram27DTCk4EhmEr_9wlAKMq4pWtnkTTjCC8eiyOoiZEA-GkUiQ4-4jDbLySCGjzM1BQM";
+
+const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const accessToken = oAuth2Client.getAccessToken();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        type: "OAuth2",
+        user: "datblu2003@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
+    },
+});
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash("error");
@@ -92,6 +124,15 @@ exports.postSignup = (req, res, next) => {
                 })
                 .then(result => {
                     res.redirect("/login");
+                    return transporter.sendMail({
+                        from: "datblu2003@gmail.com",
+                        to: email,
+                        subject: "Signup succeeded!",
+                        html: "<h1>You successfully signed up !</h1>",
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
                 });
         })
         .catch(err => {
